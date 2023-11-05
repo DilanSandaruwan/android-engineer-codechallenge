108bc5977ce45e92aa32699e56ccaef623649580
DilanSandaruwan <mnd.sanda94@gmail.com>
2023-11-05 22:34:07 +0530

    add navigation to the repository

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/views/fragments/RepoDetailsFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/views/fragments/RepoDetailsFragment.kt
@@ -3,10 +3,14 @@
  */
 package jp.co.yumemi.android.code_check.views.fragments
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -15,6 +19,8 @@ import coil.transform.CircleCropTransformation
 import dagger.hilt.android.AndroidEntryPoint
 import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.databinding.FragmentRepoDetailsBinding
+import jp.co.yumemi.android.code_check.util.components.CustomDialogFragment
+import jp.co.yumemi.android.code_check.util.exceptions.CustomErrorMessage
 import jp.co.yumemi.android.code_check.viewmodels.RepoDetailsViewModel
 
 /**
@@ -59,12 +65,47 @@ class RepoDetailsFragment : Fragment() {
         viewModel.setRepoDetails(args.repository)
 
         // Observe changes in the repository details and update the UI
-        viewModel.gitHubRepoDetails.observe(viewLifecycleOwner) {
-            binding.ownerIconView.load(it.owner?.avatarUrl) {
+        viewModel.gitHubRepoDetails.observe(viewLifecycleOwner) { gitHubAccount ->
+            binding.ownerIconView.load(gitHubAccount.owner?.avatarUrl) {
                 crossfade(true)
                 placeholder(R.drawable.placeholder_repository)
                 transformations(CircleCropTransformation())
             }
+
+            // Add an OnClickListener to the floating action button
+            binding.navFloatingActionButton.setOnClickListener {
+                val url = gitHubAccount.htmlUrl
+                if(gitHubAccount.htmlUrl.isNullOrBlank()){
+                    showErrorDialog(getString(R.string.url_not_found, requireContext()))
+                } else {
+                    openUrlInBrowser(url!!)
+                }
+            }
         }
+    }
+
+    /**
+     * Opens a URL in a web browser.
+     *
+     * @param url The URL to open.
+     */
+    private fun openUrlInBrowser(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        val chooser = Intent.createChooser(intent, getString(R.string.select_a_browser))
+        startActivity(chooser)
+    }
+
+    private fun showErrorDialog(errMsg: String) {
+        val dialog = CustomDialogFragment.newInstance(
+            title = getString(R.string.error_title),
+            message = errMsg,
+            positiveText = getString(R.string.response_ok),
+            negativeText = "",
+            positiveClickListener = { },
+            negativeClickListener = { },
+            iconResId = R.drawable.ic_dialog_error
+        )
+        dialog.show(childFragmentManager, "custom_dialog_error_details")
+
     }
 }
