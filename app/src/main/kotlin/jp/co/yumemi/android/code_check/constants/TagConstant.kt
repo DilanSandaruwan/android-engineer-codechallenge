e0aeeff0c29b38ba8da185a8cad50d6a05267c9e
DilanSandaruwan <mnd.sanda94@gmail.com>
2023-11-11 15:56:12 +0530

    code readability improvement - RepoSearchFragment

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/constants/TagConstant.kt
@@ -0,0 +1,6 @@
+package jp.co.yumemi.android.code_check.constants
+
+object TagConstant {
+    const val NO_TERM_SEARCH_DIALOG_TAG = "custom_dialog_no_term_search"
+    const val ERROR_DIALOG_TAG = "custom_dialog_error"
+}
