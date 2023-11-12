5c0d91e5df32537f6a74143d5a021fac3d295749
DilanSandaruwan <mnd.sanda94@gmail.com>
2023-11-12 22:11:36 +0530

    modify readme file

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/constants/TagConstant.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/constants/TagConstant.kt
@@ -5,4 +5,10 @@ object TagConstant {
     const val NO_TERM_SEARCH_DIALOG_TAG = "custom_dialog_no_term_search"
     const val ERROR_DIALOG_DETAILS_TAG = "custom_dialog_error_details"
     const val ERROR_DIALOG_TAG = "custom_dialog_error"
+
+    /****** dialog fragment ******/
+    const val DIALOG_FRAGMENT_TITLE = "Dialog Title"
+    const val DIALOG_FRAGMENT_MESSAGE = "Dialog Message"
+    const val DIALOG_FRAGMENT_OK = "OK"
+    const val DIALOG_FRAGMENT_CANCEL = "Cancel"
 }
